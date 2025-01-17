9f7786b4148312693d5856dfee9925c757379e46
okonayakodji <okonayakodji@mail.ru>
2025-01-18 01:40:09 +0300

    Add comment to `DEFAULT_RECHEK_TIMER` in `config.py`

--- src/config.py
+++ src/config.py
@@ -4,6 +4,9 @@ Internal app config.
 
 DEFAULT_PATH_TO_CONFIG = "./collect.yaml"
 LOGGING_FORMAT = "%(asctime)s [%(levelname)s]: %(message)s"
+
+# Timer for rechecking the readiness of plugins.
+# It should be specified in seconds.
 DEFAULT_RECHECK_TIMER = 3
 
 # Field in the yaml configuration file that will be used
